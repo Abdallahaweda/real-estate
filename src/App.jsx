83b406c0f14c463ad7c9d53cb21f5b08dc8b75f1
Abdallahaweda <abdallahslahaweda@gmail.com>
2024-05-15 05:58:24 -0700

    update the header and make th profile page privet

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import{BrowserRouter,Routes,Route} from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import Profile from "./pages/Profile"
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import PrivetRoute from "./components/PrivetRoute";
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
-        
+        <Route element={<PrivetRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
-      </BrowserRouter>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
